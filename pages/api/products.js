4158249e11b81492156f77f4836b0ea2e9f32bb1
Jen Bird <jen.b@jbcole.co.uk>
2022-08-23 23:27:52 +0100

    Commented out firebase in api

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,14 +1,14 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../firebase.config";
+// import { collection, getDocs } from "firebase/firestore";
+// import { db } from "../../firebase.config";
 // testing function hitting firebase API, works fine!
 export default async function handler(req, res) {
-  const querySnapshot = await getDocs(collection(db, "products"));
+  // const querySnapshot = await getDocs(collection(db, "products"));
 
-  const products = [];
+  // const products = [];
 
-  querySnapshot.forEach((doc) => {
-    products.push(doc.data());
-  });
+  // querySnapshot.forEach((doc) => {
+  //   products.push(doc.data());
+  // });
 
   res.status(200).json({ products });
 }
