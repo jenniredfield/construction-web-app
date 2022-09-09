4b63a1f2bab8b2b520aa0d45d449f4e1ca7495ad
Jen Bird <jen.b@jbcole.co.uk>
2022-09-09 09:12:12 +0100

    Added basic search

--- src/components/Common/NavBar/NavBar.jsx
+++ src/components/Common/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Input, Typography } from "@mui/material";
 import { NavBarContainer, NavBarWrapper } from "./NavBar.styles";
+import Search from "../Search/Search";
 
 const NavBar = () => {
   return (
@@ -10,6 +11,7 @@ const NavBar = () => {
           <Typography variant="h1"></Typography>
         </Box>
         <Box display="flex" alignItems="center">
+          <Search />
           <Box mr={2}>
             <Link href="/login">
               <Button variant="text">Login</Button>
---
+++ src/components/Common/Search/Search.jsx
@@ -0,0 +1,11 @@
+import { Box, TextField } from "@mui/material";
+
+const Search = () => {
+  return (
+    <Box>
+      <TextField placeholder="Buscar..." />
+    </Box>
+  );
+};
+
+export default Search;
